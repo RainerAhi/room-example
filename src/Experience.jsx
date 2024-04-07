b47373d7b0ba0c219badad22b255434c6c71abff
StudenxFinance <studenxfinance@gmail.com>
2024-04-07 18:18:17 +0300

    colors

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,17 +1,16 @@
 import { Environment, SoftShadows, ScrollControls } from '@react-three/drei'
 import Model from './Model'
 import { Suspense } from 'react'
-import { Loading } from './Loading'
-import { Bloom, EffectComposer, N8AO, SSAO, Vignette } from '@react-three/postprocessing'
+import { EffectComposer, N8AO } from '@react-three/postprocessing'
 
 export default function Experience() {
 
   return (
     <>
       <SoftShadows intensity={ 1 } />
-      <Suspense fallback={ <Loading /> }  >
+      <Suspense   >
         <ScrollControls damping={0.5} maxSpeed={0.5} pages={10}>
-            <Model position={ [ 0, -2, 0 ] } rotation={ [ 0, 0, 0 ] } />
+            <Model position={ [ 0, -2, 0 ] } />
         </ScrollControls>
       </Suspense>
       <EffectComposer disableNormalPass >
@@ -19,14 +18,14 @@ export default function Experience() {
       </EffectComposer>
       <Environment files="./env.hdr" background />
       <directionalLight 
-      color={ "#F7F5D0" }
+      color={ "#ECECCF" }
       shadow-bias={-0.001} castShadow position={[10, 35, -25]} 
       intensity={1} shadow-mapSize={2048} shadow-mapSize-width={4096} 
       shadow-mapSize-height={4096} shadow-camera-near={0.1} shadow-camera-far={100} 
       >
         <orthographicCamera attach="shadow-camera" args={[-25, 25, -25, 25, 0.1, 100]} />
       </directionalLight>
-      <ambientLight color={ "blue" } intensity={ 0.1 } />
+      <ambientLight color={ "blue" } intensity={ 0.13 } />
       </>
   )
 }
