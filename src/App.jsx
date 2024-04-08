2254e13b4eae7459cbbd723eccbebef32d29fc08
StudenxFinance <studenxfinance@gmail.com>
2024-04-08 17:11:13 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { Loader } from "@react-three/drei";
 import CanvasContainer from "./CanvasContainer";
+
 import { useProgress } from "@react-three/drei";
+import { Suspense, useEffect, useRef } from "react";
 
 const LoadingScreen = () => {
   const { progress, active } = useProgress();
@@ -7,7 +10,7 @@ const LoadingScreen = () => {
   return (
     <div className={`loading-screen ${active ? "" : "loading-screen--hidden"}`}>
       <div className="loading-screen__container">
-        <h1 className="loading-screen__title">3D ROOM</h1>
+        <h1 className="loading-screen__title">SODA</h1>
         <div className="progress__container">
           <div
             className="progress__bar"
