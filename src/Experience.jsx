566143fed5fffa6acfc7e262ef2d1beadc028fcd
StudenxFinance <studenxfinance@gmail.com>
2024-04-08 17:18:58 +0300

    orangeish

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -9,7 +9,7 @@ export default function Experience() {
   return (
     <>
       <SoftShadows intensity={ 1 } />
-      <Suspense fallback={ <Loading /> }  >
+      <Suspense fallback  >
         <ScrollControls damping={0.5} maxSpeed={0.5} pages={10}>
             <Model position={ [ 0, -2, 0 ] } />
         </ScrollControls>
@@ -19,7 +19,7 @@ export default function Experience() {
       </EffectComposer>
       <Environment files="./env4.hdr" background />
       <directionalLight 
-      color={ "#ECECCF" }
+      color={ "#F0E6BF" }
       shadow-bias={-0.001} castShadow position={[10, 35, -25]} 
       intensity={1.4} shadow-mapSize={2048} shadow-mapSize-width={4096} 
       shadow-mapSize-height={4096} shadow-camera-near={0.1} shadow-camera-far={100} 
