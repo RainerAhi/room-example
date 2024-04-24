67a0da6779ae3a55b9482e6fd0d4e1545891ad13
StudenxFinance <studenxfinance@gmail.com>
2024-04-24 18:33:02 +0300

    flickering improvement 2

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -11,20 +11,18 @@ export default function Experience() {
       <Suspense fallback  >
         <ScrollControls damping={1} maxSpeed={0.5} pages={10}>
             <Model position={ [ 0, -2, 0 ] } />
-            <Environment files="./env4.hdr" background />
         </ScrollControls>
       </Suspense>
       <directionalLight 
-      color={ "#FCEBE4" }
-      shadow-bias={-0.0004} castShadow position={[10, 35, -25]} 
-      intensity={1} shadow-mapSize={2048} shadow-mapSize-width={4096} 
-      shadow-mapSize-height={4096} shadow-camera-near={0.1} shadow-camera-far={100} 
+        color={ "#FCEBE4" } shadow-bias={-0.0004} castShadow position={[10, 35, -25]} 
+        intensity={1} shadow-mapSize-width={4096} shadow-mapSize-height={4096} 
       >
         <orthographicCamera attach="shadow-camera" args={[-25, 25, -25, 25, 0.1, 100]} />
       </directionalLight>
       <EffectComposer disableNormalPass >
-        <N8AO aoRadius={20} distanceFalloff={0.5} intensity={2} screenSpaceRadius quality='high' halfRes  />
+        <N8AO aoRadius={20} distanceFalloff={0.5} intensity={2} screenSpaceRadius quality="high" halfRes  />
       </EffectComposer>
+      <Environment files="./env4.hdr" background />
 
       </>
   )
